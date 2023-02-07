6485d0f593c6a9167b7d8554861df47f5e1596f2
MichelleMullane <106880578+MichelleMullane@users.noreply.github.com>
2023-02-07 15:16:56 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -6,28 +6,30 @@ import "./App.css";
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>React Weather App</h1>
-      </header>
-      <Weather />
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://chimerical-kleicha-dda4cf.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Michelle Mullane
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/MichelleMullane/Weather-React"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="App-container">
+        <header className="App-header">
+          <h1>React Weather App</h1>
+        </header>
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://chimerical-kleicha-dda4cf.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Michelle Mullane
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/MichelleMullane/Weather-React"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 };
