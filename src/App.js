5d56e98a759d6cc9eccf8cd74e02bab0019b8842
MichelleMullane <106880578+MichelleMullane@users.noreply.github.com>
2023-02-07 15:06:34 -0500

    added link to portfolio and github repo

--- src/App.js
+++ src/App.js
@@ -8,8 +8,26 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <h1>React Weather App</h1>
-        <Weather />
       </header>
+      <Weather />
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://chimerical-kleicha-dda4cf.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Michelle Mullane
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/MichelleMullane/Weather-React"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 };
