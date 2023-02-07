f3c55ca6982f27bfe1ff0a5c0c7e911c1023ca93
MichelleMullane <106880578+MichelleMullane@users.noreply.github.com>
2023-02-07 14:57:38 -0500

    removed icons, unwanted code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Weather from "./Weather";
 
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -12,6 +12,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
